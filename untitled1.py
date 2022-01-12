c786c36f789652f1f56940cbfad9d9effe9fbd88
Miyakawa Takuya <omyakawa@gmail.com>
2022-01-12 13:16:25 +0900

    first commit

---
+++ untitled1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 13:03:45 2022
+
+@author: miyagawatakuya
+"""
+
+print("Hello Python!")
+print("Do you want to know Matrix....？？")
